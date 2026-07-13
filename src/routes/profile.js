1feceb3fa57c7f9d1d789b2b511f0b8a9b7b64ef
Seyan Howard <tapzy.app@gmail.com>
2026-07-13 19:24:20 -0400

    Move stories and posts into routes folder

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -7930,8 +7930,6 @@ router.get("/u/:username", async (req, res) => {
 
         storiesBottomNav: true,
 
-        storiesTopNavActive: "discover",
-
         metaDescription: `${displayName} on Tapzy Network™. View profile, quick share links, stories, and contact details.`,
 
       })
